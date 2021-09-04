f9c9d6c971121b7052b9de96a39224bc3031912d
prietoana321 <gilsebastian505@gmail.com>
2021-09-04 13:29:41 -0500

    Lectura de api para la creacion de cartas

--- servicio.js
+++ servicio.js
@@ -1,6 +1,6 @@
 let uri = "https://api.spotify.com/v1/artists/60d24wfXkVzDSfLS6hyCjZ/top-tracks?market=US";
 
-let token = "Bearer BQCYm0ewlgruOTzrxD-LrdWg4DSaLSx1j5v_Xa7pddaNEHJP39yxJUyL-ge6hq-j-Dj5gBOBeoMBDWR2k53cXfF427z8cXP51QvWMGI61YfC3DODGVWXDVsZX83NEZvhbPGLuoxtsW-gHp7gpdPf1xko3ybZeWPRyZxXXqU";
+let token = "Bearer BQBHavAF23PxbFX4PeyM7rFQZH3cVmpr0hm5C1GjNDUnwxhuNW7y0Y6AyG-1sUfiH152wdTzc2cXc0vbweHn11IkoI39VPZ9MGSN_i0jX6D3SK5zcoGJIbq9ia6oWI71EYiHEmmxdIuc94qz1CsL_lMMFauaU5Nl8UAG6bw";
 
 let parametrosPeticion={
     method : "GET",
@@ -8,14 +8,59 @@ let parametrosPeticion={
         Authorization : token
     }
 }
-
+// Fetch es una promesa, esta promesa puede no cumplirse esa es la razón del catch
+// Esto es para la programación async (asincrona), para que el código pueda seguir con las otras tareas
 fetch(uri,parametrosPeticion)
 .then(function (respuesta) {
    return( respuesta.json());
 })
 .then(function(respuesta) {
-    console.log(respuesta);
+    console.log(respuesta);//objeto
+    pintarDatos(respuesta.tracks);
+    // console.log(respuesta.tracks);//objeto
+    // console.log(respuesta.tracks[0]);//Arreglo
+    // console.log(respuesta.tracks[0].name);//Arreglo
+    // console.log(respuesta.tracks[0].preview_url);//Arreglo
+    // console.log(respuesta.tracks[0].album.images[0].url);//Arreglo
 })
 .catch(function(error) {
     console.log(error);
 })
+function pintarDatos(datos){
+    let fila = document.getElementById("fila");
+    datos.forEach(function (cancion) {
+        let columna = document.createElement("div");
+        columna.classList.add("col");
+        let tarjeta = document.createElement("div");
+        tarjeta.classList.add("card");
+        tarjeta.classList.add("h-100");
+        let imagen = document.createElement("img");
+        imagen.classList.add("card-img-top");
+        imagen.src=cancion.album.images[0].url;
+        let cuerpo = document.createElement("div");
+        cuerpo.classList.add("card-body");
+        let titulo = document.createElement("h5");
+        titulo.classList.add("card-title");
+        titulo.innerHTML = cancion.name;
+        let descripcion = document.createElement("p");
+        descripcion.classList.add("card-text");
+        descripcion.innerHTML = cancion.name;
+        let pieCarta = document.createElement("div");
+        pieCarta.classList.add("card-footer");
+        let audio = document.createElement("audio");
+        audio.classList.add("w-100");
+        audio.setAttribute("controls","");
+        audio.src=cancion.preview_url;
+        // PADRES E HIJOS 
+        // Se empieza de adentro a afuera, comenzando por la imagen porque es lo que van en el centro
+        tarjeta.appendChild(imagen);
+        tarjeta.appendChild(titulo);
+        tarjeta.appendChild(descripcion);
+        tarjeta.appendChild(pieCarta);
+        pieCarta.appendChild(audio);
+        columna.appendChild(tarjeta);
+        imagen.appendChild(cuerpo);
+        fila.appendChild(columna);
+
+    });
+}
