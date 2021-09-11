ff7b93e04ec75d7d12484524aa2f4ec05da6b63d
santyGZ9790 <santiagogomez.h63@gmail.com>
2021-09-11 13:06:09 -0500

    Se pintan las canciones en el html

--- servicio2.js
+++ servicio2.js
@@ -28,5 +28,66 @@ fetch(uri,parametrosPeticion)
 function obtenerToken(datos) {
     let token = datos.token_type+" "+datos.access_token;
     console.log(token);
+    pedirCanciones(token);
+}
+function pedirCanciones(token){
+    let uri = "https://api.spotify.com/v1/artists/60d24wfXkVzDSfLS6hyCjZ/top-tracks?market=US";
 
+    let parametrosPeticion={
+        method : "GET",
+        headers : {
+            Authorization : token
+        }
+    }
+    fetch(uri,parametrosPeticion)
+    .then(function (respuesta) {
+    return( respuesta.json());
+    })
+    .then(function(respuesta) {
+        
+        pintarDatos(respuesta.tracks);
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+}
+function pintarDatos(datos){
+    // Traversing metodo para pintar etiquetas html 
+    let fila = document.getElementById("fila");
+    datos.forEach(function (cancion) {
+        let columna = document.createElement("div");
+        columna.classList.add("col");
+        let tarjeta = document.createElement("div");
+        tarjeta.classList.add("card");
+        tarjeta.classList.add("h-150");
+        let imagen = document.createElement("img");
+        imagen.classList.add("card-img-top");
+        imagen.src=cancion.album.images[0].url;
+        let cuerpo = document.createElement("div");
+        cuerpo.classList.add("card-body");
+        let titulo = document.createElement("h5");
+        titulo.classList.add("card-title");
+        titulo.innerHTML = cancion.name;
+        let descripcion = document.createElement("p");
+        descripcion.classList.add("card-text");
+        descripcion.innerHTML = cancion.name;
+        
+        // crear audio
+        let audio = document.createElement("audio");
+        audio.classList.add("w-100");
+        audio.src=cancion.preview_url;
+        audio.setAttribute("controls","controls");
+
+        // PADRES E HIJOS 
+        // Se empieza de adentro a afuera, comenzando por la imagen porque es lo que van en el centro
+        // Agregar barra de popularidad https://getbootstrap.com/docs/5.1/components/progress/
+        tarjeta.appendChild(imagen);
+        tarjeta.appendChild(titulo);
+        tarjeta.appendChild(descripcion);
+        tarjeta.appendChild(audio);
+        columna.appendChild(tarjeta);
+        imagen.appendChild(cuerpo);
+        fila.appendChild(columna);
+
+    });
 }
